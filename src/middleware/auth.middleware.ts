f93c892ab70635121b10be13fccd87e5a7068ea8
ankit00010 <ankitmish83@gmail.com>
2026-05-31 16:10:22 +0530

    feat(auth) : Optimize  the auth middleware with admin and customer middleware

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,27 +1,52 @@
-// auth.middleware.ts
-
 import { Request, Response, NextFunction } from "express";
-
-import jwt from "jsonwebtoken";
+import jwt, { JsonWebTokenError, TokenExpiredError } from "jsonwebtoken";
 
 import { config } from "../config";
+import { UserRole } from "../modules/users/user.interface";
+import { IAuthUser } from "../types/auth.types";
 
-const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const token = req.cookies.accessToken;
+class AuthMiddleware {
+  auth = (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const token = req.cookies.accessToken;
 
-    if (!token) {
-      throw new Error("Unauthorized access");
+      if (!token) {
+        throw new Error("Access token is required");
+      }
+
+      const decoded = jwt.verify(token, config.jwtSecret) as IAuthUser;
+
+      req.user = decoded;
+
+      next();
+    } catch (error) {
+      if (error instanceof TokenExpiredError) {
+        return next(new Error("Session expired. Please login again."));
+      }
+
+      if (error instanceof JsonWebTokenError) {
+        return next(new Error("Invalid access token."));
+      }
+
+      next(error);
+    }
+  };
+
+  admin = (req: Request, res: Response, next: NextFunction) => {
+    if (req.user?.role !== UserRole.ADMIN) {
+      throw new Error("You do not have permission to access this resource.");
     }
 
-    const decoded = jwt.verify(token, config.jwtSecret);
+    next();
+  };
 
-    req.user = decoded;
+  customer = (req: Request, res: Response, next: NextFunction) => {
+    if (req.user?.role !== UserRole.CUSTOMER) {
+      throw new Error("You do not have permission to access this resource.");
+    }
 
     next();
-  } catch (error) {
-    next(error);
-  }
-};
+  };
+}
 
-export default authMiddleware;
+export const authMiddleware = new AuthMiddleware();

6defd962e5da01b28e7edb7d4ee8185b9cdd93fd
ankit00010 <ankitmish83@gmail.com>
2026-05-31 16:10:51 +0530

    feat(auth) : Optimize  the Error handling by defining the error centric utility functions

--- src/utils/AppError.ts
+++ src/utils/AppError.ts
@@ -14,4 +14,39 @@ class AppError extends Error {
   }
 }
 
+export class BadRequestError extends AppError {
+  constructor(message = "Bad request") {
+    super(message, 400);
+  }
+}
+
+export class UnauthorizedError extends AppError {
+  constructor(message = "Authentication required") {
+    super(message, 401);
+  }
+}
+
+export class ForbiddenError extends AppError {
+  constructor(message = "You do not have permission to access this resource") {
+    super(message, 403);
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(message = "Resource not found") {
+    super(message, 404);
+  }
+}
+
+export class ConflictError extends AppError {
+  constructor(message = "Resource already exists") {
+    super(message, 409);
+  }
+}
+
+export class ValidationError extends AppError {
+  constructor(message = "Validation failed") {
+    super(message, 422);
+  }
+}
 export default AppError;

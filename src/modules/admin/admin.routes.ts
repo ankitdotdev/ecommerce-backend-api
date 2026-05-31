4de0989557f94b06282e26672ca7650786fada32
ankit00010 <ankitmish83@gmail.com>
2026-05-31 16:12:58 +0530

    feat(auth) : Add auth middleware check to a protected routes

--- src/modules/admin/admin.routes.ts
+++ src/modules/admin/admin.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import authRouter from "./auth/auth.routes";
 import productRouter from "./products/products.routes";
+import { authMiddleware } from "../../middleware/auth.middleware";
 
 const adminRouter = Router();
 
 adminRouter.use("/auth", authRouter);
+
+adminRouter.use(authMiddleware.auth);
+adminRouter.use(authMiddleware.admin);
+
+
+
 adminRouter.use("/products", productRouter);
 
 export default adminRouter;

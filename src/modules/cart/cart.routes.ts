bdddc9b0e7436c09d37d452ae62b55b0f201a262
ankit00010 <ankitmish83@gmail.com>
2026-05-31 23:47:15 +0530

    feat(cart): setup cart module and schema

---
+++ src/modules/cart/cart.routes.ts
@@ -0,0 +1,15 @@
+// Cart APIs
+// Method   Endpoint                               Description
+
+// GET      /api/v1/cart                           Get current user's cart
+
+// POST     /api/v1/cart/items                     Add product to cart
+
+// PATCH    /api/v1/cart/items/:productId          Update product quantity in cart
+
+// DELETE   /api/v1/cart/items/:productId          Remove product from cart
+
+// DELETE   /api/v1/cart                           Clear entire cart
+
+
+
